60863743fac7f8c76b1392ab9b27e506598cb879
Oliver <113297002+oliver748@users.noreply.github.com>
2022-11-20 14:54:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ import os
 
 if not os.path.isdir("images"):
     os.mkdir("images")
+    os.mkdir("images/asian")
+    os.mkdir("images/white")
+    os.mkdir("images/black")
+    os.mkdir("images/latino")
+    os.mkdir("images/middle-eastern")
+    os.mkdir("images/indian")
 if not os.path.isdir("temp"):
     os.mkdir("temp")
 if not os.path.isdir("data"):
